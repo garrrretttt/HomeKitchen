7432ccbd980a787b05a87e69d54e107f9f6bf814
Garrett Honzay <garrett.honzay@ndsu.edu>
2023-04-19 20:49:46 -0500

    started meal and account interfaces

---
+++ src/meal.ts
@@ -0,0 +1,26 @@
+import { Time } from "@angular/common";
+import { Account } from "./account";
+
+export interface Meal {
+    id: number;
+    dishName: string;
+    partySize: number;
+    AmountBooked: number;
+    tags: string[];
+    dietaryRestrictions: string[];
+    cost: number;
+    location: string;
+    timeStart: Time;
+    picture: string;
+    chef: Account;
+    // Dish 
+    // Party Size 
+    // People that have booked 
+    // Children / Pet tags 
+    // Dietary Restrictions 
+    // Price 
+    // Location 
+    // Time 
+    // Picture of Meal 
+    // Chef 
+}
