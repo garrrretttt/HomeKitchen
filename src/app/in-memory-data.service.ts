83d6637bb8e52c8f860d7f1cd868ac705f0c4eb0
Garrett Honzay <garrett.honzay@ndsu.edu>
2023-04-22 19:23:12 -0500

    Made meal component look like a card for meal list Started meal list

--- src/app/in-memory-data.service.ts
+++ src/app/in-memory-data.service.ts
@@ -9,10 +9,21 @@ import { Account } from './account';
 export class InMemoryDataService implements InMemoryDbService {
   createDb() {
     const accounts = [
-      { id: 0, isChef: true, name: 'Master', dietaryRestrictions: ['None'], bio: 'I am master chef', profilePicture: '', ratings: { 'Service': [5], 'Meal Quality': [5] }, username: 'master', password: 'account' }
+      {id : 0,    isChef : true,  name : 'Master',    dietaryRestrictions : ['None'], 
+    bio : 'I am master chef', profilePicture : '', 
+    ratings : {'Diner': [5], 'Chef': [4, 5, 4]},    username: 'master',     password: 'account'},
+
+    {id : 1,    isChef : false,  name : 'Alan Turing',    dietaryRestrictions : ['None'], 
+    bio : 'I like to eat', profilePicture : '', 
+    ratings : {'Diner': [5, 3, 3], 'Chef': []},    username: 'Alan',     password: 'Turing'}
     ]
     const meals = [
-      { id: 0, dishName: 'Spaghetti', partySize: 4, AmountBooked: 0, tags: ['Child-Friendly', 'Pets Present'], dietaryRestrictions: ['None'], cost: 12, location: '1234 56th St S Fargo, ND', timeStart: '6:00', picture: '', chef: accounts[0] }
+      { id: 0, dishName: 'Spaghetti', partySize: 4,
+    AmountBooked: 0, tags: ['Child-Friendly', 'Pets Present'], 
+    dietaryRestrictions: ['None'], cost: 12, 
+    location: '1234 56th St S Fargo, ND', timeStart: '6:00', 
+    picture: 'https://i.imgur.com/e76p3L3.png', 
+    chef: accounts[0], ratings: [4, 4, 4] }
     ];
     return { meals };
   }
--- src/app/meal/meal-detail.component.ts
+++ src/app/meal/meal-detail.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Location } from '@angular/common';
 
 import { Meal } from '../meal';
@@ -8,28 +8,41 @@ import { MealService } from '../meal.service';
 @Component({
   selector: 'meal-detail',
   templateUrl: './meal-detail.component.html',
-  styleUrls: [ './meal-detail.component.css' ]
+  styleUrls: ['./meal-detail.component.css']
 })
 export class MealDetailComponent implements OnInit {
-  meal: Meal | undefined;
+  id: number = 0;
+  meal?: Meal;
+  details: boolean = false;
+  hover: boolean = false;
+  onHover: string = 'box-shadow: 5px 5px 5px gray;'
 
   constructor(
     private route: ActivatedRoute,
     private mealService: MealService,
-    private location: Location
-  ) {}
+    private location: Location,
+    private router: Router,
+  ) { }
 
   ngOnInit(): void {
     this.getMeal();
+    if (this.router.url == `/meal/view/${this.meal?.id}` || this.router.url == `/meal/edit/${this.meal?.id}`) {
+      // this.details = true;
+    }
   }
 
   getMeal(): void {
-    const id = Number(this.route.snapshot.paramMap.get('id'));
-    this.mealService.getMeal(id)
-      .subscribe(meal => this.meal = meal);
+    if (this.id) {
+      this.mealService.getMeal(0)
+        .subscribe(meal => this.meal = meal);
+    }
   }
 
   goBack(): void {
     this.location.back();
+  }
+
+  cardClick(id: number): void {
+    this.router.navigate([`/meal/view/${id}`])
   }
 }
