6c3199ef5256ef0ea7b7b0a2ee1e1584118d9347
ej-ndsu <ethan.jablon@ndsu.edu>
2023-04-27 22:40:37 -0500

    Added calendar component. Modified meal list component to take meals array as input. Nested meal list in calendar. Added filtering method to split meals into 1. future booked and created, and 2. past booked and created. Replaced meal list in router with calendar. Added mealsCreated array to account class.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { MealDetailComponent } from './meal/meal-detail.component';
 import { MealListComponent } from './meal-list/meal-list.component';
 import { EditMealComponent } from './edit-meal/edit-meal.component';
+import { CalendarComponent } from './calendar/calendar.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
@@ -10,11 +11,11 @@ const routes: Routes = [
   { path: 'meal/view/:id', component: MealDetailComponent },
   { path: 'meal/edit/:id', component: EditMealComponent },
   { path: 'meal/create', component: MealDetailComponent },
-  { path: 'meal/list', component: MealListComponent },
+  { path: 'calendar', component: CalendarComponent },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
---
+++ src/app/calendar/calendar.component.ts
@@ -0,0 +1,61 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Account } from '../account';
+import { Meal } from '../meal';
+import { MealService } from '../meal.service';
+
+@Component({
+  selector: 'calendar',
+  templateUrl: './calendar.component.html',
+  styleUrls: ['./calendar.component.css'],
+})
+export class CalendarComponent {
+  account = {
+    id: 1,
+    isChef: false,
+    name: 'Alan Turing',
+    dietaryRestrictions: ['None'],
+    bio: 'I like to eat',
+    profilePicture: '',
+    mealsBooked: [],
+    mealsCreated: [],
+    ratings: { Diner: [5, 3, 3], Chef: [] },
+    username: 'Alan',
+    password: 'Turing',
+  };
+
+  created: Meal[] = [];
+  booked: Meal[] = [];
+  createdHistory: Meal[] = [];
+  bookedHistory: Meal[] = [];
+  done: boolean = false;
+
+  constructor(private mealService: MealService) {}
+
+  ngOnInit(): void {
+    this.getMeals();
+  }
+
+  getMeals() {
+    this.mealService.getMeals().subscribe((meals) => {
+      console.log('Retrieving meals for calendar page...');
+      for (const meal of meals) {
+        if (meal.chefId == this.account?.id) {
+          if (meal.startDate >= new Date()) {
+            this.created?.push(meal);
+          } else {
+            this.createdHistory?.push(meal);
+          }
+        } else if (meal.accountsBooked.includes(this.account.id)) {
+          if (new Date(meal.startDate) >= new Date()) {
+            this.booked?.push(meal);
+          } else {
+            this.bookedHistory?.push(meal);
+          }
+        }
+        console.log(this.booked?.at(0));
+      }
+      this.done = true;
+      console.log('All meals retrieved.');
+    });
+  }
+}
