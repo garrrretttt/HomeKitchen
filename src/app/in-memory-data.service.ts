9b30b7df50940298a0d8241705327aa6195737f7
ej-ndsu <ethan.jablon@ndsu.edu>
2023-04-27 22:51:57 -0500

    Removing stupid changes from local save

--- src/app/in-memory-data.service.ts
+++ src/app/in-memory-data.service.ts
@@ -10,89 +10,149 @@ export class InMemoryDataService implements InMemoryDbService {
   createDb() {
     const accounts: Account[] = [
       {
-        id: 0, isChef: true, name: 'Master', dietaryRestrictions: ['None'],
-        bio: 'I am master chef', profilePicture: '', mealsBooked: [],
-        ratings: { 'Diner': [5], 'Chef': [4, 5, 4] }, username: 'master', password: 'account'
+        id: 0,
+        isChef: true,
+        name: 'Master',
+        dietaryRestrictions: ['None'],
+        bio: 'I am master chef',
+        profilePicture: '',
+        mealsBooked: [],
+        mealsCreated: [],
+        ratings: { Diner: [5], Chef: [4, 5, 4] },
+        username: 'master',
+        password: 'account',
       },
 
       {
-        id: 1, isChef: false, name: 'Alan Turing', dietaryRestrictions: ['None'],
-        bio: 'I like to eat', profilePicture: '', mealsBooked: [],
-        ratings: { 'Diner': [5, 3, 3], 'Chef': [] }, username: 'Alan', password: 'Turing'
-      }
-    ]
+        id: 1,
+        isChef: false,
+        name: 'Alan Turing',
+        dietaryRestrictions: ['None'],
+        bio: 'I like to eat',
+        profilePicture: '',
+        mealsBooked: [],
+        mealsCreated: [],
+        ratings: { Diner: [5, 3, 3], Chef: [] },
+        username: 'Alan',
+        password: 'Turing',
+      },
+    ];
     const meals: Meal[] = [
       {
-        id: 0, dishName: 'Spaghetti', partySize: 4,
-        accountsBooked: [], tags: ['Child-Friendly', 'Pets Present'],
-        dietaryRestrictions: [], cost: 12,
+        id: 0,
+        dishName: 'Spaghetti',
+        partySize: 4,
+        accountsBooked: [],
+        tags: ['Child-Friendly', 'Pets Present'],
+        dietaryRestrictions: [],
+        cost: 12,
         location: '1234 56th St S Fargo, ND',
-        startDate: new Date(2023, 5, 5, 17, 30), duration: 1,
+        startDate: new Date(2023, 5, 5, 17, 30),
+        duration: 1,
         picture: 'https://i.imgur.com/e76p3L3.png',
-        chefId: 0, ratings: [4, 4, 4]
+        chefId: 0,
+        ratings: [4, 4, 4],
       },
       {
-        id: 1, dishName: 'Noodles with Tomato Sauces', partySize: 4,
-        accountsBooked: [], tags: ['Child-Friendly', 'Pets Present'],
-        dietaryRestrictions: ['None'], cost: 12,
+        id: 1,
+        dishName: 'Noodles with Tomato Sauces',
+        partySize: 4,
+        accountsBooked: [],
+        tags: ['Child-Friendly', 'Pets Present'],
+        dietaryRestrictions: ['None'],
+        cost: 12,
         location: '1234 56th St S Fargo, ND',
-        startDate: new Date(2023, 5, 5, 17, 30), duration: 1,
+        startDate: new Date(2023, 5, 5, 17, 30),
+        duration: 1,
         picture: 'https://i.imgur.com/e76p3L3.png',
-        chefId: 0, ratings: [4, 4, 4]
+        chefId: 0,
+        ratings: [4, 4, 4],
       },
       {
-        id: 2, dishName: 'Red Sauce with Wormy Stuff', partySize: 4,
-        accountsBooked: [], tags: ['Child-Friendly', 'Pets Present'],
-        dietaryRestrictions: ['None'], cost: 12,
+        id: 2,
+        dishName: 'Red Sauce with Wormy Stuff',
+        partySize: 4,
+        accountsBooked: [],
+        tags: ['Child-Friendly', 'Pets Present'],
+        dietaryRestrictions: ['None'],
+        cost: 12,
         location: '1234 56th St S Fargo, ND',
-        startDate: new Date(2023, 5, 5, 17, 30), duration: 1,
+        startDate: new Date(2023, 5, 5, 17, 30),
+        duration: 1,
         picture: 'https://i.imgur.com/e76p3L3.png',
-        chefId: 0, ratings: [4, 4, 4]
+        chefId: 0,
+        ratings: [4, 4, 4],
       },
       {
-        id: 3, dishName: 'Red Sauce with Wormy Stuff asdflkjasdf ;lkjasdf;lkj', partySize: 4,
-        accountsBooked: [], tags: ['Child-Friendly', 'Pets Present'],
-        dietaryRestrictions: ['None'], cost: 12,
+        id: 3,
+        dishName: 'Red Sauce with Wormy Stuff asdflkjasdf ;lkjasdf;lkj',
+        partySize: 4,
+        accountsBooked: [],
+        tags: ['Child-Friendly', 'Pets Present'],
+        dietaryRestrictions: ['None'],
+        cost: 12,
         location: '1234 56th St S Fargo, ND',
-        startDate: new Date(2023, 5, 5, 17, 30), duration: 1,
+        startDate: new Date(2023, 5, 5, 17, 30),
+        duration: 1,
         picture: 'https://i.imgur.com/e76p3L3.png',
-        chefId: 0, ratings: [4, 4, 4]
+        chefId: 0,
+        ratings: [4, 4, 4],
       },
       {
-        id: 4, dishName: 'Red Sauce with Wormy Stuff', partySize: 4,
-        accountsBooked: [], tags: ['Child-Friendly', 'Pets Present'],
-        dietaryRestrictions: ['None'], cost: 12,
+        id: 4,
+        dishName: 'Red Sauce with Wormy Stuff',
+        partySize: 4,
+        accountsBooked: [],
+        tags: ['Child-Friendly', 'Pets Present'],
+        dietaryRestrictions: ['None'],
+        cost: 12,
         location: '1234 56th St S Fargo, ND',
-        startDate: new Date(2023, 5, 5, 17, 30), duration: 1,
+        startDate: new Date(2023, 5, 5, 17, 30),
+        duration: 1,
         picture: 'https://i.imgur.com/e76p3L3.png',
-        chefId: 0, ratings: [4, 4, 4]
+        chefId: 0,
+        ratings: [4, 4, 4],
       },
       {
-        id: 5, dishName: 'Red Sauce with Wormy Stuff', partySize: 4,
-        accountsBooked: [], tags: ['Child-Friendly', 'Pets Present'],
-        dietaryRestrictions: ['None'], cost: 12,
+        id: 5,
+        dishName: 'Red Sauce with Wormy Stuff',
+        partySize: 4,
+        accountsBooked: [],
+        tags: ['Child-Friendly', 'Pets Present'],
+        dietaryRestrictions: ['None'],
+        cost: 12,
         location: '1234 56th St S Fargo, ND',
-        startDate: new Date(2023, 5, 5, 17, 30), duration: 1,
+        startDate: new Date(2023, 5, 5, 17, 30),
+        duration: 1,
         picture: 'https://i.imgur.com/e76p3L3.png',
-        chefId: 0, ratings: [4, 4, 4]
+        chefId: 0,
+        ratings: [4, 4, 4],
       },
       {
-        id: 6, dishName: 'Red Sauce with Wormy Stuff', partySize: 4,
-        accountsBooked: [], tags: ['Child-Friendly', 'Pets Present'],
-        dietaryRestrictions: ['None'], cost: 12,
+        id: 6,
+        dishName: 'Red Sauce with Wormy Stuff',
+        partySize: 4,
+        accountsBooked: [],
+        tags: ['Child-Friendly', 'Pets Present'],
+        dietaryRestrictions: ['None'],
+        cost: 12,
         location: '1234 56th St S Fargo, ND',
-        startDate: new Date(2023, 5, 5, 17, 30), duration: 1,
+        startDate: new Date(2023, 5, 5, 17, 30),
+        duration: 1,
         picture: 'https://i.imgur.com/e76p3L3.png',
-        chefId: 0, ratings: [4, 4, 4]
-      }
+        chefId: 0,
+        ratings: [4, 4, 4],
+      },
     ];
     return { meals };
   }
 
   genId(meals: Meal[]): number {
-    return Math.max(...meals.map(meal => meal.id)) + 1;
+    return Math.max(...meals.map((meal) => meal.id)) + 1;
   }
   genAccountId(acounts: Account[]): number {
-    return acounts.length > 0 ? Math.max(...acounts.map(acount => acount.id)) + 1 : 11;
+    return acounts.length > 0
+      ? Math.max(...acounts.map((acount) => acount.id)) + 1
+      : 11;
   }
 }
