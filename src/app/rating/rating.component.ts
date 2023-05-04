f8afa41020dfee772b1b06f3e3fb6854cdc3c24f
Garrett Honzay <garrett.honzay@ndsu.edu>
2023-05-03 19:46:31 -0500

    added ratings to chef accounts

--- src/app/rating/rating.component.ts
+++ src/app/rating/rating.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Rating } from '../rating';
+import { AccountService } from '../account.service';
+import { Account } from '../account';
 
 @Component({
-  selector: 'app-rating',
+  selector: 'rating',
   templateUrl: './rating.component.html',
   styleUrls: ['./rating.component.css']
 })
 export class RatingComponent {
+
+  @Input() rating?: Rating;
+  raterAccount?: Account;
+
+  constructor(
+    private accountService: AccountService,
+  ){}
+
+    ngOnInit(){
+      if(this.rating){
+        this.accountService.getAccount(this.rating.raterUid).then(account => {
+          this.raterAccount = account;
+        });
+      }
+    }
 
 }
