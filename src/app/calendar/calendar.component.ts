e6eb7683b0bd3676e7f30f795872a186a3a2ac7f
ej-ndsu <ethan.jablon@ndsu.edu>
2023-05-05 14:16:48 -0500

    Added import

--- src/app/calendar/calendar.component.ts
+++ src/app/calendar/calendar.component.ts
@@ -2,7 +2,9 @@ import { Component, Input, OnInit } from '@angular/core';
 import { Meal } from '../meal';
 import { MealService } from '../meal.service';
 import { AccountService } from '../account.service';
-import { Query, getDocs } from 'firebase/firestore';
+import { Query, getDocs, orderBy, query } from 'firebase/firestore';
+import { MatDialog } from '@angular/material/dialog';
+import { FilterDialogComponent, FilterDialogInput } from '../filter-dialog/filter-dialog.component';
 
 @Component({
   selector: 'calendar',
@@ -17,7 +19,7 @@ export class CalendarComponent implements OnInit {
   bookedHistory: Meal[] = [];
   done: boolean = false;
 
-  constructor(private mealService: MealService, private accountService: AccountService) {}
+  constructor(private mealService: MealService, private accountService: AccountService, public dialog: MatDialog) {}
 
   ngOnInit(): void {
     this.getMeals();
@@ -49,16 +51,27 @@ export class CalendarComponent implements OnInit {
   }
 
   async filterCreatedHistory(): Promise<void> {
-   /* const dialogRef = this.dialog.open(ContactDialogComponent, {
-      width: '300px',
-      data: this.clone(contact),
+    this.createdHistory = [];
+    let input: FilterDialogInput = {
+      query: ,
+      isExplore: false,
+    };
+    const dialogRef = this.dialog.open(FilterDialogComponent, {
+      width: '500px',
+      data: {  },
     });
 
     dialogRef.afterClosed().subscribe((result: Query<Meal[]>) => {
-      this.createdHistory = await(getDocs(result));
+      let meals: Meal[] = [];
+      let q = query(this.mealRef, orderBy('startDate'));
+      let querySnapshot = await getDocs(q);
+      querySnapshot.forEach((doc) => {
+        meals.push(doc.data() as Meal);
+      });
+      this.createdHistory = meals;
     }); 
 
-    const querySnapshot = await getDocs(q);*/
+    const querySnapshot = await getDocs(result);
   }
 
   filter(): void {
